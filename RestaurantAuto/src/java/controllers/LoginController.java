d0fa0cdc8b9bda5df84267899f97112eb876d453
hoanhtung <hoanhtung.xy@gmail.com>
2017-07-06 15:08:13 +0700

    a

--- RestaurantAuto/src/java/controllers/LoginController.java
+++ RestaurantAuto/src/java/controllers/LoginController.java
@@ -39,42 +39,21 @@ public class LoginController extends HttpServlet {
 
             TrungBean bean = new TrungBean();
             String role = bean.getRole(id, password);
-<<<<<<< HEAD
             if (!role.equals("false")) {
                 if (role.equals("Cook")) {
                     url = "LoadFoodsController";
-                } else {
-                    url = "ShowTableStatusController";
-                }
-                if (role.equals("Manager")) {
+                } else if (role.equals("Manager")) {
                     url = "ManagerController";
-                } else if (role.equals("2")) {
-                    url = "tableStatus.jsp";
-                    if (!bean.getRole(id, password).equals("false")) {
-                        url = "ShowTableStatusController";
-
-                        //lưu role vs staffId trong session cho dễ sử dụng sau này
-                        request.getSession(true).setAttribute("ROLE", bean.getRole(id, password));
-                        request.getSession().setAttribute("STAFFID", id);
-                    } else {
-                        request.setAttribute("ERROR", "WRONG PASSWORD OR USERNAME");
-                    }
-=======
-            if (role.equals("Manager")) {
-                url = "ManagerController";
-            } else if (!role.equals("false")) {
-                url = "tableStatus.jsp";
-                if (!bean.getRole(id, password).equals("false")) {
-                    url = "ShowTableStatusController";
-
-                    //lưu role vs staffId trong session cho dễ sử dụng sau này
-                    request.getSession(true).setAttribute("ROLE", bean.getRole(id, password));
-                    request.getSession().setAttribute("STAFFID", id);
                 } else {
-                    request.setAttribute("ERROR", "WRONG PASSWORD OR USERNAME");
->>>>>>> a3a05e5a6a7f06939a248567c449fd14fa0356ea
+                    url = "ShowTableStatusController";
                 }
+                //lưu role vs staffId trong session cho dễ sử dụng sau này
+                request.getSession(true).setAttribute("ROLE", bean.getRole(id, password));
+                request.getSession().setAttribute("STAFFID", id);
+            } else {
+                request.setAttribute("ERROR", "WRONG PASSWORD OR USERNAME");
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -122,4 +101,3 @@ public class LoginController extends HttpServlet {
     }// </editor-fold>
 
 }
-
